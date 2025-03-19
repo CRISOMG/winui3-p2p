b94577930e6b648c42cd9f0beee3c2fb23f61450
Cristian <cristiancaraballo112@gmail.com>
2025-03-19 02:35:37 -0400

    test

--- p2p/Enums.cs
+++ p2p/Enums.cs
@@ -8,7 +8,7 @@ namespace p2p.Enums
 {
     public static class MyStringList
     {
-        public static string String1 { get; } = "p2p.Pages.SocketMessagesPage";
-        public static string String2 { get; } = "p2p.Pages.HomePage";
+        public static string String1 { get; } = "test 1";
+        public static string String2 { get; } = "test 2";
     }
 }
